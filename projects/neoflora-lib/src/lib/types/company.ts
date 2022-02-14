f9cfdc96f2f9a59d2216aac3ef67f9fe4231d853
tahoni <tahoni@gmail.com>
2022-02-14 19:48:25 +0200

    Remove unused logo base64

--- projects/neoflora-lib/src/lib/types/company.ts
+++ projects/neoflora-lib/src/lib/types/company.ts
@@ -3,14 +3,12 @@ export class Company {
   private readonly _legalName: string;
   private readonly _registrationNumber: string;
   private readonly _logo: string;
-  private readonly _base64: string;
 
-  constructor(name: string, legalName: string, registrationNumber: string, logo: string, base64: string) {
+  constructor(name: string, legalName: string, registrationNumber: string, logo: string) {
     this._name = name;
     this._legalName = legalName;
     this._registrationNumber = registrationNumber;
     this._logo = logo;
-    this._base64 = base64;
   }
 
   get name(): string {
@@ -27,9 +25,5 @@ export class Company {
 
   get logo(): string {
     return this._logo;
-  }
-
-  get base64(): string {
-    return this._base64;
   }
 }
