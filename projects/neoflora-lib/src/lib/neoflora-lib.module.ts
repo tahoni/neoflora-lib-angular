6e9e09edcbd6d7c20148b5fa3cd9f59e54a6c54f
tahoni <tahoni@gmail.com>
2022-02-18 22:42:28 +0200

    Add providers to module

--- projects/neoflora-lib/src/lib/neoflora-lib.module.ts
+++ projects/neoflora-lib/src/lib/neoflora-lib.module.ts
@@ -2,11 +2,19 @@ import {NgModule} from '@angular/core';
 import {FooterComponent} from './form/footer/footer.component';
 import {HeaderComponent} from './form/header/header.component';
 import {TahoniLibModule} from "@tahoni/tahoni-lib";
+import {ImageService} from "./utils/image.service";
+import {FileService} from "./utils/file.service";
+import {IconService} from "./utils/icon.service";
 
 @NgModule({
   declarations: [
     FooterComponent,
     HeaderComponent,
+  ],
+  providers: [
+    IconService,
+    FileService,
+    ImageService,
   ],
   imports: [
     TahoniLibModule,
