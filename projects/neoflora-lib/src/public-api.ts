515234ffd2c4d30472b9b0944cf496b7ca37ba95
tahoni <tahoni@gmail.com>
2022-02-19 17:33:31 +0200

    Export services

--- projects/neoflora-lib/src/public-api.ts
+++ projects/neoflora-lib/src/public-api.ts
@@ -4,9 +4,11 @@
 
 export * from './lib/neoflora-lib.module';
 
+/* Components */
 export * from './lib/form/header/header.component';
 export * from './lib/form/footer/footer.component';
 
+/* Services */
 export * from './lib/utils/icon.service';
 export * from './lib/utils/file.service';
 export * from './lib/utils/image.service';
