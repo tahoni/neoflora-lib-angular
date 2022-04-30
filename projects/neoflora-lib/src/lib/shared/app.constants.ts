909362f5120f2a86ccb709b0a292dfc68511912d
tahoni <tahoni@gmail.com>
2022-04-30 19:45:17 +0200

    Remove business registration of Neoflora

--- projects/neoflora-lib/src/lib/shared/app.constants.ts
+++ projects/neoflora-lib/src/lib/shared/app.constants.ts
@@ -2,7 +2,7 @@ import {Company} from "../types/company.type";
 
 export class AppConstants {
   private static readonly _company =
-    new Company('Neoflora', 'Neoflora (Pty) Ltd', '76/01503/07', 'neoflora.png')
+    new Company('Neoflora', 'Neoflora (Pty) Ltd', '', 'neoflora.png')
 
   static get company(): any {
     return this._company;
